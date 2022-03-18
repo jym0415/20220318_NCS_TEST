62566d8cb2e6ba6a80789725d98fd6ec5c3c9d09
jym0415 <jym9504@gmail.com>
2022-03-18 12:06:16 +0900

    db셋팅

--- routes/getsend.js
+++ routes/getsend.js
@@ -1,8 +1,32 @@
 var express = require('express');
+var mysql = require('mysql');
+var dbconfig = require('../db/config.js')
+const connection = mysql.createConnection(dbconfig)
 var router = express.Router()
 
 router.get('/',(req,res) => {
     res.send('getsend요청응답')
 })
+
+router.get('/getjson',(req,res) => {
+    res.send({'url': '/getsend/getjson'});
+})
+
+router.post('/post',(req,res) => {
+    res.send({
+        'url' : 'getsend/post',
+        'title':'getsend.js안의post라우팅'
+    })
+}) //post는 복잡한 내용을 담음
+
+router.get('/awssql',(req, res) => {
+    connection.query('SELECT * FROM reactinterview.interview', (error, rows) => {
+        if(error) throw error;
+        console.log('해당스키마안의 테이블 내용 : ', rows);
+        res.send(rows);
+    });
+})
+
+
 
 module.exports = router;
--- routes/postsend.js
+++ routes/postsend.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router()
+//post는 주소창으로안나옴 postman으로 확인하기!
 
 router.post('/',(req,res) => {
     res.send('post요청응답')
